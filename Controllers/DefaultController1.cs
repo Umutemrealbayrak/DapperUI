219e14244cf789013d8bf13e182beecfb2d4e586
Umutemrealbayrak <96690901+Umutemrealbayrak@users.noreply.github.com>
2024-11-25 23:54:04 +0300

    Add files via upload

---
+++ Controllers/DefaultController1.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace DapperUI.Controllers
+{
+    public class DefaultController1 : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ Dtos/ProductDtos/ResultProductDto.cs
@@ -0,0 +1,12 @@
+﻿namespace DapperUI.Dtos.ProductDtos
+{
+    public class ResultProductDto
+    {
+        public int productID { get; set; }
+        public string title { get; set; }
+        public decimal price { get; set; }
+        public string city { get; set; }
+        public string district { get; set; }
+        
+    }
+}
